5220d604c0b5a2a6a81bef79d99b60f1fe370324
Your Grandma <dmytro.chalyi.corp@gmail.com>
2017-11-12 13:05:34 +0200

    refactored v2

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"encoding/csv"
 	"bufio"
 	"io"
+	"fmt"
 )
 
 type Person struct {
@@ -28,11 +29,11 @@ func main() {
 	var activemembers []Person
 	//зчитуємо дані в слайс структур
 	for {
-		line, error := reader.Read()
-		if error == io.EOF {
+		line, err := reader.Read()
+		if err == io.EOF {
 			break
-		} else if error != nil {
-			log.Fatal(error)
+		} else if err != nil {
+			log.Fatal(err)
 		}
 		activemembers = append(activemembers, Person{
 			lastname: line[0],
@@ -58,6 +59,7 @@ func main() {
 	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
 	ucfg.Timeout = 60
 	updates, _ := bot.GetUpdatesChan(ucfg)
+	var msg tgbotapi.Chattable
 	// читаем обновления из канала
 	for {
 		for update := range updates {
@@ -65,27 +67,33 @@ func main() {
 			if update.Message == nil {
 				continue
 			}
-			UserName := update.Message.From.UserName
+			//UserName := update.Message.From.UserName
 
 			// ID чата/диалога.
 			// Может быть идентификатором как чата с пользователем
 			// (тогда он равен UserID) так и публичного чата/канала
-			ChatID := update.Message.Chat.ID
+			currentChat := update.Message.Chat
 
 			// Текст сообщения
-			Text := update.Message.Text
+			//Text := update.Message.Text
 
-			log.Printf("[%s] %d %s", UserName, ChatID, Text)
+			//log.Printf("[%s] %d %s", UserName, ChatID, Text)
 
 			// в зависимости от команды - выводим имя из мапки или всю инфу
 			switch update.Message.Command() {
 			case "get":
+				fmt.Printf(update.Message.CommandArguments())
+				if update.Message.CommandArguments() == "" {
+					msg=tgbotapi.NewMessage(currentChat.ID,"Empty command")
+					bot.Send(msg)
+					continue
+				}
 				reply := mapmap[update.Message.CommandArguments()].info
-				msg := tgbotapi.NewMessage(ChatID, reply)
+				msg = tgbotapi.NewMessage(currentChat.ID, reply)
 				bot.Send(msg)
 			case "all":
 				for i := 0; i < len(activemembers); i++ {
-					bot.Send(tgbotapi.NewMessage(ChatID, "Name: "+activemembers[i].lastname+"; Phone: "+activemembers[i].info))
+					bot.Send(tgbotapi.NewMessage(currentChat.ID, "Name: "+activemembers[i].lastname+"; Phone: "+activemembers[i].info))
 				}
 			}
 
